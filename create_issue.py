731b4757aa3c27bb0d75ba2136ec5d8541c808be
George Kaimakis <geoptus@gmail.com>
2018-08-30 00:21:50 +0000

    First draft of bot script:
    this first draft creates an issue at [mariatta's] strange-relationship
    repo.

---
+++ create_issue.py
@@ -0,0 +1,26 @@
+"""
+a github bot that,
+greets people who create issues in my project,
+says thanks when a pull request has been closed,
+applies a label to issues or pull requests,
+gives a thumbs up reaction to comments I make
+"""
+
+import asyncio
+import os
+import aiohttp
+from gidgethub.aiohttp import GitHubAPI
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        gh = GitHubAPI(session, "geokai", oauth_token=os.getenv("GH_AUTH"))
+        await gh.post('/repos/mariatta/strange-relationship/issues',
+              data={
+                  'title': 'My bot likes to smile',
+                  'body': 'geokai says, Use more emoji!',
+              })
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
