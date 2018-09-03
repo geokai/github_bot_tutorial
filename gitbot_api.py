90c7d4f4c5a2e766c76ba4203985482e7f8669ba
George Kaimakis <geoptus@gmail.com>
2018-09-03 19:40:19 +0000

    Adds 'Gitbot_api' class:

--- gitbot_api.py
+++ gitbot_api.py
@@ -10,29 +10,29 @@ from gidgethub.aiohttp import GitHubAPI
 
  
  class Gitbot_api(object):
-     """ Gitbot_api class provides the methods required to interact with
+     """Gitbot_api class provides the methods required to interact with
      the github api.
      """
 
     def __init__(self):
-        """ initialize an instant of the api bot """
-        pass()
+        """initialize an instant of the api bot"""
+        pass
 
     def create_issue(self):
-        """ method to create an issue """
-        pass()
+        """method to create an issue"""
+        pass
 
     def comment_issue(self):
-        """ method to issue a comment to an existing issue """
-        pass()
+        """method to issue a comment to an existing issue"""
+        pass
 
     def close_issue(self):
-        """ method to close an issu """
-        pass()
+        """method to close an issu"""
+        pass
 
     def send_reaction_issue(self):
-        """ method to send a reaction to an existing issue """
+        """method to send a reaction to an existing issue"""
 
     def get_reaction_issue(self):
-        """ method to retriev a reaction from an existing issue """
-        pass()
+        """method to retriev a reaction from an existing issue"""
+        pass
